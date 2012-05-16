01f9b88aa07800ee8f6c15de60eeb22b14b85477
7vsy <masato.watanabe.mail@gmail.com>
2012-05-17 02:42:13 +0900

    bug fixed: callback logic

--- jquery.enqt.js
+++ jquery.enqt.js
@@ -1,6 +1,6 @@
 /*!
- * jQuery.enqt - 
- * Version: 0.1.2
+ * jQuery.enqt 
+ * Version: 0.1.3
  * https://github.com/7vsy/jQuery.enqt
  *
  * Copyright(c) 2012 Masato WATANABE <7vsyml@gmail.com>
@@ -29,11 +29,23 @@
 
   var isTranslate3d = /Android 4/i.test(navigator.userAgent) ? true : false;
 
+  var joinTransformValues = function( obj ){
+    var joinString = '';
+    for( var key in obj ){
+      joinString += obj[key] + ' ';
+    }
+    return joinString;
+  }
 
   $.fn[pluginName] = function( duration, delay, styles, callback ) {  
 
     var totalDelay = duration + delay;
-    var transformMethods = new Array();
+    var transformValues = { 
+      'translate' : 'translate(0px, 0px)',
+      'scale' : 'scale(1, 1)',
+      'rotate' : 'rotate(0deg)',
+      'skew' : 'skewX(0deg) skewY(0deg)'
+    };
 
     $.each(useCss3Properties, function() {
       if ( styles[this] ){
@@ -41,52 +53,45 @@
       }
     });
 
-    for (var methodName in styles ){
-      if ( methodName === 'translate' ){
-        var posX = styles['translate']['x'] || '0px';
-        var posY = styles['translate']['y'] || '0px';
-        if ( isTranslate3d ){
-          transformMethods.push(' translate3d(' + posX + ', ' + posY + ', 0px ) ');
-        }else{
-          transformMethods.push(' translate(' + posX + ', ' + posY + ' ) ');
+    for (var tfValue in transformValues ){
+      if ( styles[tfValue] ){
+        if ( tfValue === 'transform' ){
+          break;
+        } else if ( tfValue === 'translate' ){
+          var posX = styles['translate']['x'] || '0px';
+          var posY = styles['translate']['y'] || '0px';
+          if ( isTranslate3d ){
+            transformValues[tfValue] = 'translate3d(' + posX + ', ' + posY + ', 0px )';
+          }else{
+            transformValues[tfValue] = 'translate(' + posX + ', ' + posY + ' )';
+          }
+        } else if ( tfValue === 'scale' ){
+          var scaleX = styles['scale']['x'] || 1;
+          var scaleY = styles['scale']['y'] || 1;
+          transformValues[tfValue] = 'scale(' + scaleX + ', ' + scaleY + ' )';
+        } else if ( tfValue === 'rotate' ){
+          var rotate = styles['rotate'] || '0deg';
+          transformValues[tfValue] = 'rotate(' + rotate + ' )';
+        } else if ( tfValue === 'skew' ){
+          var skewX = styles['skew']['x'] || '0deg';
+          var skewY = styles['skew']['y'] || '0deg';
+          transformValues[tfValue] = 'skewX(' + skewX + ') skewY(' + skewY + ' )';
         }
-      } else if ( methodName === 'scale' ){
-        var scaleX = styles['scale']['x'] || 1;
-        var scaleY = styles['scale']['y'] || 1;
-        transformMethods.push(' scale(' + scaleX + ', ' + scaleY + ' ) ');
-      } else if ( methodName === 'rotate' ){
-        var rotate = styles['rotate'] || '0deg';
-        transformMethods.push(' rotate(' + rotate + ' ) ');
-      } else if ( methodName === 'skew' ){
-        var skewX = styles['skew']['x'] || '0deg';
-        var skewY = styles['skew']['y'] || '0deg';
-        transformMethods.push(' skewX(' + skewX + ') skewY(' + skewY + ' ) ');
-      } else if ( methodName === 'transform' ){
-        transformMethods.push( styles['transform'] );
       }
     }
-
-    styles[vendorProperties['transform']] = transformMethods.join(' ') || '';
-
-    if ( styles[vendorProperties['transform']].indexOf("scale") === -1 ){
-      styles[vendorProperties['transform']] += ' scale(1,1) ';
-    }
-    if ( styles[vendorProperties['transform']].indexOf("rotate") === -1 ){
-      styles[vendorProperties['transform']] += ' rotate(0deg) ';
-    }
-    if ( styles[vendorProperties['transform']].indexOf("skew") === -1 ){
-      styles[vendorProperties['transform']] += ' skewX(0deg) skewY(0deg) ';
-    }
+    
+    styles[vendorProperties['transform']] = styles['transform'] || joinTransformValues( transformValues ) || '';
 
     return this.each(function(i,elem) {
 
-      if ( callback ){
-        setTimeout(function(){
-          callback();
-        }, totalDelay );
-      }
-
       $(this).queue(function(){ 
+
+        if ( callback ){
+          setTimeout(function(){
+            callback();
+          }, totalDelay );
+        }
+
         $(this)
           .css( vendorProperties.transitionDuration, duration + 'ms' )
           .css( vendorProperties.transitionDelay, delay + 'ms' )
